bac4f2a7d5cb3566b1b2c61432e64d92c329db5f
Lucas Viga <lucasviga12@gmail.com>
2021-03-17 01:39:49 -0400

    split application into 2 components: sidebar and content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface GenresProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+}
+
+interface GenreIdProps {
+  selectedGenreId: number;
+}
+
+interface ButtonProps {
+  handleClickButton: () => number;
+}
+
+export function SideBar({genres, selectedGenreId, handleClickButton}: GenresProps & GenreIdProps & ButtonProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
